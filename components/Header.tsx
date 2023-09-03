8269066616ce493fea53d0634f7fb19ecb7062eb
Vivacoda <75243836+vivacoda@users.noreply.github.com>
2023-09-03 23:39:34 +0700

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,7 @@ import ThemeSwitch from './ThemeSwitch'
 import SearchButton from './SearchButton'
 import Image from 'next/image'
 
-const Header = () => {
+const Header = ({ menuData }) => {
   return (
     <header className="flex items-center justify-between py-4 w-full fixed top-0 left-0 right-0 bg-white dark:bg-black z-50 container mx-auto">
       <div className="">
@@ -42,6 +42,41 @@ const Header = () => {
               {link.title}
             </Link>
           ))}
+        <div className="relative inline-block text-left">
+          <button className="inline-flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-black rounded-md hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-white">
+            Cities
+          </button>
+          <div className="absolute right-0 w-56 mt-2 origin-top-right bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5">
+            <div className="px-1 py-1 ">
+              {menuData.map((cityData) => (
+                <div className="relative" key={cityData.city}>
+                  <button className="group w-full px-4 py-2 text-sm text-gray-900 hover:bg-gray-100 hover:text-gray-900">
+                    {cityData.city}
+                  </button>
+                  <div className="absolute left-0 w-56 mt-2 origin-top-right bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5">
+                    {cityData.areas.map((areaData) => (
+                      <div className="relative" key={areaData.area}>
+                        <button className="group w-full px-4 py-2 text-sm text-gray-900 hover:bg-gray-100 hover:text-gray-900">
+                          {areaData.area}
+                        </button>
+                        <div className="absolute left-0 w-56 mt-2 origin-top-right bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5">
+                          {areaData.categories.map((category) => (
+                            <button
+                              key={category}
+                              className="w-full px-4 py-2 text-sm text-gray-900 hover:bg-gray-100 hover:text-gray-900"
+                            >
+                              {category}
+                            </button>
+                          ))}
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
         <SearchButton />
         <ThemeSwitch />
         <MobileNav />
