9d3dff201743977f3e27e3667cb35d85df51d00d
Vivacoda <75243836+vivacoda@users.noreply.github.com>
2023-09-03 17:37:08 +0700

    Clean Metadata

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -7,12 +7,12 @@ const siteMetadata = {
     'Explore the best of Thailand’s travel destinations and vibrant nightlife with Thai Spirit. From bustling Bangkok to serene beaches, we cover it all.',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://thai-spirit.com',
+  siteUrl: 'https://thaispirit.net',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
   siteLogo: '/static/images/Thai-spirit.png',
   socialBanner: '/static/images/Thai-spirit.png',
   mastodon: 'https://mastodon.social/@mastodonuser',
-  email: 'contact@thai-spirit.com',
+  email: 'contact@thaispirit.net',
   github: 'https://github.com',
   twitter: 'https://twitter.com/Twitter',
   facebook: 'https://facebook.com',
@@ -23,10 +23,10 @@ const siteMetadata = {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
     // supports Plausible, Simple Analytics, Umami, Posthog or Google Analytics.
-    umamiAnalytics: {
-      // We use an env variable for this site to avoid other users cloning our analytics ID
-      umamiWebsiteId: process.env.NEXT_UMAMI_ID, // e.g. 123e4567-e89b-12d3-a456-426614174000
-    },
+    //umamiAnalytics: {
+    // We use an env variable for this site to avoid other users cloning our analytics ID
+    // umamiWebsiteId: process.env.NEXT_UMAMI_ID, // e.g. 123e4567-e89b-12d3-a456-426614174000
+    // },
     // plausibleAnalytics: {
     //   plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
     // },
@@ -34,59 +34,58 @@ const siteMetadata = {
     // posthogAnalytics: {
     //   posthogProjectApiKey: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
     // },
-    // googleAnalytics: {
-    //   googleAnalyticsId: '', // e.g. G-XXXXXXX
-    // },
-  },
-  newsletter: {
-    // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus
-    // Please add your .env file and modify it according to your selection
-    provider: 'buttondown',
-  },
-  comments: {
-    // If you want to use an analytics provider you have to add it to the
-    // content security policy in the `next.config.js` file.
-    // Select a provider and use the environment variables associated to it
-    // https://vercel.com/docs/environment-variables
-    provider: 'giscus', // supported providers: giscus, utterances, disqus
-    giscusConfig: {
-      // Visit the link below, and follow the steps in the 'configuration' section
-      // https://giscus.app/
-      repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
-      repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
-      category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
-      categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
-      mapping: 'pathname', // supported options: pathname, url, title
-      reactions: '1', // Emoji reactions: 1 = enable / 0 = disable
-      // Send discussion metadata periodically to the parent window: 1 = enable / 0 = disable
-      metadata: '0',
-      // theme example: light, dark, dark_dimmed, dark_high_contrast
-      // transparent_dark, preferred_color_scheme, custom
-      theme: 'light',
-      // theme when dark mode
-      darkTheme: 'transparent_dark',
-      // If the theme option above is set to 'custom`
-      // please provide a link below to your custom theme css file.
-      // example: https://giscus.app/themes/custom_example.css
-      themeURL: '',
-      // This corresponds to the `data-lang="en"` in giscus's configurations
-      lang: 'en',
+    googleAnalytics: {
+      googleAnalyticsId: process.env.NEXT_GA,
     },
-  },
-  search: {
-    provider: 'kbar', // kbar or algolia
-    kbarConfig: {
-      searchDocumentsPath: 'search.json', // path to load documents to search
+    newsletter: {
+      // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus
+      // Please add your .env file and modify it according to your selection
+      provider: 'buttondown',
     },
-    // provider: 'algolia',
-    // algoliaConfig: {
-    //   // The application ID provided by Algolia
-    //   appId: 'R2IYF7ETH7',
-    //   // Public API key: it is safe to commit it
-    //   apiKey: '599cec31baffa4868cae4e79f180729b',
-    //   indexName: 'docsearch',
-    // },
+    comments: {
+      // If you want to use an analytics provider you have to add it to the
+      // content security policy in the `next.config.js` file.
+      // Select a provider and use the environment variables associated to it
+      // https://vercel.com/docs/environment-variables
+      provider: 'utterances', // supported providers: giscus, utterances, disqus
+      giscusConfig: {
+        // Visit the link below, and follow the steps in the 'configuration' section
+        // https://giscus.app/
+        repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
+        //repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
+        // category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
+        // categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
+        mapping: 'pathname', // supported options: pathname, url, title
+        reactions: '1', // Emoji reactions: 1 = enable / 0 = disable
+        // Send discussion metadata periodically to the parent window: 1 = enable / 0 = disable
+        metadata: '0',
+        // theme example: light, dark, dark_dimmed, dark_high_contrast
+        // transparent_dark, preferred_color_scheme, custom
+        theme: 'github-light',
+        // theme when dark mode
+        darkTheme: 'github-dark',
+        // If the theme option above is set to 'custom`
+        // please provide a link below to your custom theme css file.
+        // example: https://giscus.app/themes/custom_example.css
+        themeURL: '',
+        // This corresponds to the `data-lang="en"` in giscus's configurations
+        lang: 'en',
+      },
+    },
+    search: {
+      provider: 'kbar', // kbar or algolia
+      kbarConfig: {
+        searchDocumentsPath: 'search.json', // path to load documents to search
+      },
+      // provider: 'algolia',
+      // algoliaConfig: {
+      //   // The application ID provided by Algolia
+      //   appId: 'R2IYF7ETH7',
+      //   // Public API key: it is safe to commit it
+      //   apiKey: '599cec31baffa4868cae4e79f180729b',
+      //   indexName: 'docsearch',
+      // },
+    },
   },
 }
-
 module.exports = siteMetadata
